19aa721cf230065f3e95a0aeda2516a33557ff06
sheffykhlg <56907336+sheffykhlg@users.noreply.github.com>
2023-07-14 14:28:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,12 @@
-gunicorn app:app & python3 bot.py
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/sheffykhlg/2ndmoviescrown /DQ-The-File-Donor
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+fi
+cd /DQ-The-File-Donor
+pip3 install -U -r requirements.txt
+echo "Starting DQ-The-File-Donor...."
+python3 bot.py
