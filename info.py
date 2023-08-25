2e80a15db4e390462518838fd6df2e235d2f7d9b
niteshkumar46 <132472966+niteshkumar46@users.noreply.github.com>
2023-08-25 20:23:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', False))
+VERIFY = bool(environ.get('VERIFY', True))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'tnshort.net')
 SHORTLINK_API = environ.get('SHORTLINK_API', '47243a67843f8210409e41c2a31667c2db2f4d1d')
 SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', 'earn4share.online')
